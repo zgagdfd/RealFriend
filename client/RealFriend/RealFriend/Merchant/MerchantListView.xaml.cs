151a8f833d7d2d450fe9c1a786c747520959591b
jason <lvshilonghebut@163.com>
2018-06-12 10:24:15 +0800

    add web request for message list and merchant list

--- client/RealFriend/RealFriend/Merchant/MerchantListView.xaml.cs
+++ client/RealFriend/RealFriend/Merchant/MerchantListView.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,35 +23,68 @@ namespace RealFriend
         private List<MerchantData> LoadMerchantData()
         {
             List<MerchantData> list = new List<MerchantData>();
-            StringBuilder stringBuilder = new StringBuilder();
-            foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
+            string url = "http://real.chinanorth.cloudapp.chinacloudapi.cn/merchant";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            string statusCode = response.StatusCode.ToString();
+            if (response.IsSuccessStatusCode)
             {
-                if (info.IsPublic && info.IsStatic && info.FieldType == typeof(Color))
+                string result = response.Content.ReadAsStringAsync().Result;
+                List<Merchant> merchants_result = JsonConvert.DeserializeObject<List<Merchant>>(result);
+                foreach (Merchant item in merchants_result)
                 {
-                    string name = info.Name;
-                    stringBuilder.Clear();
-                    int index = 0;
-                    foreach (char c in name)
-                    {
-                        if (index != 0 && Char.IsUpper(c))
-                        {
-                            stringBuilder.Append(' ');
-                        }
-                        stringBuilder.Append(c);
-                        ++index;
-                    }
-                    string url = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31";
+                    string image_url = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31";
                     Random random = new Random();
                     int OrderQty = random.Next(500);
                     double Grade = random.NextDouble() * 5;
                     int AvgConsumption = random.Next(15, 200);
                     String Contact = "18141912818";
-                    String Location = "北大东南门对面计算机研究所大楼2层";
-                    MerchantData data = new MerchantData(url, name, OrderQty, Grade, AvgConsumption, Contact, Location);
-                    list.Add(data);
+                    //String Location = "北大东南门对面计算机研究所大楼2层";
+                    string location = item.info;
+                    string merchant_name = item.name;
+
+                    MerchantData data = new MerchantData(image_url, merchant_name, OrderQty, Grade, AvgConsumption, Contact, location);
                 }
             }
+
+
+
+
+
             return list;
+
+
+
+
+            //StringBuilder stringBuilder = new StringBuilder();
+            //foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
+            //{
+            //    if (info.IsPublic && info.IsStatic && info.FieldType == typeof(Color))
+            //    {
+            //        string name = info.Name;
+            //        stringBuilder.Clear();
+            //        int index = 0;
+            //        foreach (char c in name)
+            //        {
+            //            if (index != 0 && Char.IsUpper(c))
+            //            {
+            //                stringBuilder.Append(' ');
+            //            }
+            //            stringBuilder.Append(c);
+            //            ++index;
+            //        }
+            //        string url = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31";
+            //        Random random = new Random();
+            //        int OrderQty = random.Next(500);
+            //        double Grade = random.NextDouble() * 5;
+            //        int AvgConsumption = random.Next(15, 200);
+            //        String Contact = "18141912818";
+            //        String Location = "北大东南门对面计算机研究所大楼2层";
+            //        MerchantData data = new MerchantData(url, name, OrderQty, Grade, AvgConsumption, Contact, Location);
+            //        list.Add(data);
+            //    }
+            //}
+            //return list;
         }
     }
 
@@ -74,4 +108,14 @@ namespace RealFriend
         public String Contact { get; private set; }
         public String Location { get; private set; }
     }
+
+    public class Merchant{
+        public string name { get; set; }
+        public string image { get; set; }
+        public string info { get; set; }
+        public string introduction { get; set; }
+        public int id { get; set; }
+    }
+
+
 }
--- client/RealFriend/RealFriend/Message/MessageListView.xaml.cs
+++ client/RealFriend/RealFriend/Message/MessageListView.xaml.cs
@@ -1,50 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace RealFriend
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MessageListView : ContentView
-	{
-		public MessageListView ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessageListView : ContentView
+    {
+        public MessageListView()
+        {
+            InitializeComponent();
             messageListView.ItemsSource = LoadMessageData();
         }
 
         private List<MessageData> LoadMessageData()
         {
             List<MessageData> list = new List<MessageData>();
-            StringBuilder stringBuilder = new StringBuilder();
-            foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
+            string url = "http://real.chinanorth.cloudapp.chinacloudapi.cn/message";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            string statusCode = response.StatusCode.ToString();
+            if (response.IsSuccessStatusCode)
             {
-                if (info.IsPublic && info.IsStatic && info.FieldType == typeof(Color))
+                string result = response.Content.ReadAsStringAsync().Result;
+                List<message> test_message = JsonConvert.DeserializeObject<List<message>>(result);
+                foreach (message item in test_message)
                 {
-                    string name = info.Name;
-                    stringBuilder.Clear();
-                    int index = 0;
-                    foreach (char c in name)
-                    {
-                        if (index != 0 && Char.IsUpper(c))
-                        {
-                            stringBuilder.Append(' ');
-                        }
-                        stringBuilder.Append(c);
-                        ++index;
-                    }
+                    //头像链接暂时API未提供，  title不需要，api需给出发送者姓名，先暂时用type代替显示
                     String imageUrl = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31";
-                    MessageData data = new MessageData(imageUrl, name, stringBuilder.ToString());
+                    MessageData data = new MessageData(imageUrl, item.type, item.content);
                     list.Add(data);
                 }
             }
             return list;
+
+
+
+
+
+            //StringBuilder stringBuilder = new StringBuilder();
+            //foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
+            //{
+            //    if (info.IsPublic && info.IsStatic && info.FieldType == typeof(Color))
+            //    {
+            //        string name = info.Name;
+            //        stringBuilder.Clear();
+            //        int index = 0;
+            //        foreach (char c in name)
+            //        {
+            //            if (index != 0 && Char.IsUpper(c))
+            //            {
+            //                stringBuilder.Append(' ');
+            //            }
+            //            stringBuilder.Append(c);
+            //            ++index;
+            //        }
+            //        String imageUrl = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31";
+            //        MessageData data = new MessageData(imageUrl, name, stringBuilder.ToString());
+            //        list.Add(data);
+            //    }
+            //}
+            //return list;
         }
     }
 
@@ -63,5 +86,12 @@ namespace RealFriend
         public string Name { private set; get; }
         public string Desc { private set; get; }
         public string Time { private set; get; }
+    }
+
+    public class message
+    {
+        public string content { get; set; }
+        public string create_time { get; set; }
+        public string type { get; set; }
     }
 }
