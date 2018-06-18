e8083690fc528ae96ff83867f103c4c2323ba0e1
BigLiam <radishk4@live.cn>
2018-06-18 16:31:16 +0800

    add Splash Screen

--- client/RealFriend/RealFriend.Android/MainActivity.cs
+++ client/RealFriend/RealFriend.Android/MainActivity.cs
@@ -9,11 +9,15 @@ using Android.OS;
 
 namespace RealFriend.Droid
 {
-    [Activity(Label = "RealFriend", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "RealFriend", Icon = "@drawable/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            // base.Window.RequestFeature(WindowFeatures.ActionBar);
+            System.Threading.Thread.Sleep(2000);
+            base.SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
---
+++ client/RealFriend/RealFriend.Android/SplashActivity.cs
@@ -0,0 +1,17 @@
+﻿
+using Android.App;
+using Android.OS;
+
+namespace RealFriend.Droid
+{
+    [Activity(Label = "SplashActivity", MainLauncher = false, NoHistory = true, Theme = "@style/Theme.Splash")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(2000);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
